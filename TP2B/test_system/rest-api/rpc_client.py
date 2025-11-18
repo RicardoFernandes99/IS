12e7a0b9ecc1e2a86d0944fcaf31c2bb9a9bcb92
RicardoFernandes99 <filipericardo7@hotmail.com>
2025-11-18 21:57:21 +0000

    fix

--- TP2B/test_system/rest-api/rpc_client.py
+++ TP2B/test_system/rest-api/rpc_client.py
@@ -2,8 +2,8 @@ from xmlrpc.client import ServerProxy
 
 rpc = ServerProxy("http://rpc-server:8000/RPC2", allow_none=True)
 
-def convert_csv(csv_bytes):
-    return rpc.convert_csv(csv_bytes)
+def convert_csv(csv_bytes, root_name="root", row_name="row"):
+    return rpc.convert_csv(csv_bytes, root_name, row_name)
 
 def insert_xml(xml_string):
     return rpc.insert_xml(xml_string)
