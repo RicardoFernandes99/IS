59bfefe30ca6cc18f2574722dea3448df0480830
RicardoFernandes99 <filipericardo7@hotmail.com>
2025-11-21 23:48:28 +0000

    ..

--- Integração de Sistemas/Desafio 1/csv_to_xml.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('idealista_houses.csv')
-df.to_xml('idealista_houses.xml', index=False)
--- Integração de Sistemas/Desafio 1/schema_xml_validator.py
+++
@@ -1,4 +0,0 @@
-import xmlschema
-schema = xmlschema.XMLSchema('idealista_houses.xsd')
-is_valid = schema.is_valid('idealista_houses.xml')
-print(f'XML is valid: {is_valid}')
